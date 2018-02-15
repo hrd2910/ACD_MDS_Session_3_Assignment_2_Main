2b5bc8e232c2f19e30a2c0a1a5688876a3c83156
hrd2910 <35139001+hrd2910@users.noreply.github.com>
2018-02-16 01:58:54 +0530

    Add files via upload

---
+++ listComprehension.py
@@ -0,0 +1,8 @@
+# Write List comprehensions to produce the following Lists
+# ['A', 'C', 'A', 'D', 'G', 'I', ’L’, ‘ D’]
+
+listCom = []
+for letter in 'ACADGILD':
+    listCom.append(letter)
+print(listCom)
+
