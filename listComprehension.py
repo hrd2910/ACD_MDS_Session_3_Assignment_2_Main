2781a401ce3ce3918297dfd843e84b1f4c8e2f78
hrd2910 <35139001+hrd2910@users.noreply.github.com>
2018-02-16 02:12:42 +0530

    Update listComprehension.py

--- listComprehension.py
+++ listComprehension.py
@@ -6,3 +6,12 @@ for letter in 'ACADGILD':
     listCom.append(letter)
 print(listCom)
 
+
+
+# Write List comprehensions to produce the following Lists
+# [[2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7], [5, 6, 7, 8]]
+
+
+matrix = [[2, 3, 4, 5],[3,4,5,6], [4,5,6,7],[5,6,7,8]]
+transpose = [[row[i] for row in matrix] for i in range(4)]
+print (transpose)
